678483701bec0e0931ab926991a016356df4fbb2
Ido Sagiv <ido.sagiv@mail.huji.ac.il>
2021-05-21 14:22:11 +0300

    initial commit

---
+++ app/src/main/java/idosa/huji/postpc/sandwich_stand/MainActivity.kt
@@ -0,0 +1,11 @@
+package idosa.huji.postpc.sandwich_stand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/idosa/huji/postpc/sandwich_stand/SandwichStandApp.kt
@@ -0,0 +1,10 @@
+package idosa.huji.postpc.sandwich_stand
+
+import android.app.Application
+
+class SandwichStandApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // todo: lunch local and remote DBs
+    }
+}
