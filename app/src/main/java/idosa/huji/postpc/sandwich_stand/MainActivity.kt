10ee80e96ddec3fd59ac71bd69f4d9c953e0540d
Ido Sagiv <ido.sagiv@mail.huji.ac.il>
2021-06-01 22:24:29 +0300

    when app launch, navigate to the correct screen

--- app/src/main/java/idosa/huji/postpc/sandwich_stand/MainActivity.kt
+++ app/src/main/java/idosa/huji/postpc/sandwich_stand/MainActivity.kt
@@ -1,11 +1,29 @@
 package idosa.huji.postpc.sandwich_stand
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // todo: check the current order status and open the relevant screen
+        when (SandwichStandApp.getLocalDb().currentOrder?.status) {
+            null -> {
+                startActivity(Intent(this, NewOrderActivity::class.java))
+            }
+            SandwichOrder.OrderStatus.WAITING -> {
+                startActivity(Intent(this, EditOrderActivity::class.java))
+            }
+            SandwichOrder.OrderStatus.IN_PROGRESS -> {
+                startActivity(Intent(this, WaitForOrderActivity::class.java))
+            }
+            SandwichOrder.OrderStatus.READY -> {
+                startActivity(Intent(this, OrderReadyActivity::class.java))
+            }
+            else -> {
+                // todo: verify tis is what should be done
+                startActivity(Intent(this, NewOrderActivity::class.java))
+            }
+        }
     }
 }
